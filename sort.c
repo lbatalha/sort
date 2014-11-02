3620ba407e7cd4809391427ea18b9fb7f98f7e78
lbatalha <lfrb25@gmail.com>
2014-11-02 03:53:18 +0000

    Test1

--- sort.c
+++ sort.c
@@ -6,32 +6,35 @@
 int main(int argc, char *argv[])
 {
 	char line[MAXCHAR];
-	/*char *lines = NULL;*/
-	char lines[2];
-	char hello[] = "hello";
-	int i;
+	char *lines = NULL;
+/*	char lines[2];
+	char hello[] = "hello";*/
+	int i = 1;
+	int linecount=0;
 
 	FILE *fp = fopen(argv[1], "r");
 
 	while(fgets(line, MAXCHAR, fp) != NULL)
 	{
+		linecount = linecount + 1;
+
+		lines = realloc(lines, MAXCHAR);
+
+		strcpy(lines, line);
+
+		lines = lines + MAXCHAR;
 		
-		/*lines = realloc(lines, MAXCHAR);*/
-
-
-
-		
-
-
-
-
-
-
 	}
 
 
+	for(i=0; i < linecount; i++)
+	{
+		lines = lines + MAXCHAR*i;
+		strcpy(line, lines);
+		printf("%s\n", line);
+	}
 
-
+	fclose(fp);
 
 	return 0;
 }
